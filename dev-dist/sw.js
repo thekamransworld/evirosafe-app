9aa83b117450c37b433ae0de5d6c7a8deeb95c0e
Mahmoud Zeena <mahmoud123@gmail.com>
2025-12-30 11:42:46 +0300

    Add missing PDF and Excel export dependencies

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9gvd5af5c9s"
+    "revision": "0.51ioq88n6k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
